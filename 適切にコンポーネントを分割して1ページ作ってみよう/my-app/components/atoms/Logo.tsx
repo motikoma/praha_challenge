3afa3c4fc3a6d613e4de579b921dc6146904cbcf
motikoma <motikoma5125@gmail.com>
2023-05-13 17:58:11 +0900

    fix: ロゴ修正

--- 適切にコンポーネントを分割して1ページ作ってみよう/my-app/components/atoms/Logo.tsx
+++ 適切にコンポーネントを分割して1ページ作ってみよう/my-app/components/atoms/Logo.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 export const Logo = () => {
   return (
-    <Image src='/logo.svg' alt='Acme Store' width={32} height={32} priority />
+    <Link href='/'>
+      <Image src='/logo.svg' alt='Acme Store' width={32} height={32} priority />
+    </Link>
   );
 };
