0d5cf113e7de9f2830b712b63461e504ee7c1ef4
sakamoto keisuke <keisuke.sakamoto@pol.co.jp>
2023-06-18 17:25:45 +0900

    mswから値を取得する形式に変更

--- SWR_React Queryを理解する/sample/.storybook/preview.ts
+++ SWR_React Queryを理解する/sample/.storybook/preview.ts
@@ -1,4 +1,27 @@
 import type { Preview } from "@storybook/react";
+import { rest } from "msw";
+import { setupWorker } from "msw";
+
+export const handlers = [
+  rest.get(
+    "https://api.github.com/repos/tannerlinsley/react-query",
+    (req, res, ctx) => {
+      return res(
+        ctx.json({
+          name: "react-query",
+          description:
+            "Hooks for fetching, caching and updating asynchronous data in React",
+          subscribers_count: 100,
+          stargazers_count: 200,
+          forks_count: 300,
+        })
+      );
+    }
+  ),
+];
+
+const worker = setupWorker(...handlers);
+worker.start();
 
 const preview: Preview = {
   parameters: {
--- SWR_React Queryを理解する/sample/src/components/github/index.tsx
+++ SWR_React Queryを理解する/sample/src/components/github/index.tsx
@@ -1,4 +1,4 @@
-import { fetcher } from "@/fetcher";
+import { fetcher } from "../../fetcher";
 import useSWR from "swr";
 
 interface RepoData {
