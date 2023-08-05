e47f44f9401eb2973dfdb1912c2d20b8ac2cda5f
sakamoto keisuke <keisuke.sakamoto@pol.co.jp>
2023-08-05 13:01:43 +0900

    fix: Googleアカウントによるログイン

---
+++ Firebase Authentication（IDaaS）を使ってみる/issue.md
@@ -0,0 +1,5 @@
+TODO
+
+- Pages Router でトライしてみる
+
+参考情報：https://qiita.com/masakiwakabayashi/items/741998ed5b830d8f3707#firebase%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96
---
+++ Firebase Authentication（IDaaS）を使ってみる/my-app/src/app/login/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { getAuth } from "firebase/auth";
+import { login, logout } from "../../lib/login";
+
+export default function Login() {
+  const handleClickLogin = () => {
+    login();
+  };
+  const handleClickLogout = () => {
+    logout();
+  };
+  const handleClickLoginCheck = () => {
+    const auth = getAuth();
+    if (auth.currentUser) {
+      alert(`${auth.currentUser?.displayName}でログインしてます。`);
+    } else {
+      alert("ログインしてないです。");
+    }
+  };
+
+  const handleFetch = async () => {
+    const res = await fetch("/api/hello");
+    const json = await res.json();
+    console.log(json);
+  };
+
+  return (
+    <div className='w-64 m-auto'>
+      <h2>Login</h2>
+      <div>
+        <button onClick={handleClickLogin}>Googleでログイン</button>
+      </div>
+      <div>
+        <button onClick={handleClickLogout}>ログアウト</button>
+      </div>
+      <div>
+        <button onClick={handleClickLoginCheck}>ログインチェック</button>
+      </div>
+      <div>
+        <button onClick={handleFetch}>APIリクエスト</button>
+      </div>
+    </div>
+  );
+}
